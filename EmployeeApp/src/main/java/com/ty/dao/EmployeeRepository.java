003b0b991067ddff2edef8dfd3b30d16c543d1f2
hashna-dev <hashnamansoor16@gmail.com>
2025-03-04 16:11:58 +0530

    Delete EmployeeApp directory

--- EmployeeApp/src/main/java/com/ty/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ty.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ty.dto.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-
-}
